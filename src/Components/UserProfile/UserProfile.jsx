fa32d73a413370df72e8265d3708ba271035b1d4
Mario Espada <mario.espada77@gmail.com>
2021-02-25 00:35:16 +0100

    CU-eq5ccg - primera version perfil usuario

--- src/Components/UserProfile/UserProfile.jsx
+++ src/Components/UserProfile/UserProfile.jsx
@@ -1,11 +1,69 @@
 import React, { Component } from 'react';
+import './css/userProfile.css'
 
 class UserProfile extends Component {
     state = {  }
     render() { 
         return (
-            <div>
-                <p>User Profile</p>
+            <div className="user-profile-wrapp">
+                {/* Inicio Menú  */}
+                <div className="user-profile-menu">
+                    <div className="sidebar-menu">
+                        <div className="menu-content">
+                            <div className="menu-search">
+                                <p>Search</p>
+                            </div>
+                            <div className="menu-user-card">
+                                <p>User Card</p>
+                            </div>
+                            <div className="menu-options">
+                                <p>Menu</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/* Inicio contenido del perfil  */}
+                <div className="user-profile-content">
+                    <div className="prueba">
+                        <div className="user-profile-scroll">
+                            <p>User Profile</p>
+                            <p>User Profile</p>
+                            <p>User Profile</p>
+                            <p>User Profile</p>
+                            <p>User Profile</p>
+                            <p>User Profile</p>
+                            <p>User Profile</p>
+                            <p>User Profile</p>
+                            <p>User Profile</p>
+                            <p>User Profile</p>
+                            <p>User Profile</p>
+                            <p>User Profile</p>
+                            <p>User Profile</p>
+                            <p>User Profile</p>
+                            <p>User Profile</p>
+                            <p>User Profile</p>
+                            <p>User Profile</p>
+                            <p>User Profile</p>
+                            <p>User Profile</p>
+                            <p>User Profile</p>
+                            <p>User Profile</p>
+                            <p>User Profile</p>
+                            <p>User Profile</p>
+                            <p>User Profile</p>
+                            <p>User Profile</p>
+                            <p>User Profile</p>
+                            <p>User Profile</p>
+                            <p>User Profile</p>
+                            <p>User Profile</p>
+                            <p>User Profile</p>
+                            <p>User Profile</p>
+                            <p>User Profile</p>
+                            <p>User Profile</p>
+                            <p>User Profile</p>
+                            
+                        </div>
+                    </div>
+                </div>
             </div>
          );
     }
